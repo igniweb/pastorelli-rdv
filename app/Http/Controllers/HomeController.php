4b2e3f40fcf4dac014ae3792b1f180cfc6a219ab
Sébastien Muller <igniweb.net@gmail.com>
2015-06-18 23:52:35 +0200

    Functionnal broadcast system with Pusher

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,7 +27,9 @@ class HomeController extends Controller {
         ]);
         event(new RdvIsSaved($rdv));
 
-        return view('home.index');
+        $rdvs = Rdv::all();
+
+        return view('home.index', compact('rdvs'));
     }
 
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,18 +1,30 @@
 @extends('layouts.default')
 
 @section('main')
-Yo broadcast
+<h2>Yo broadcast</h2>
+<ul id="rdvs">
+    @foreach ($rdvs as $rdv)
+        <li>{{ $rdv->label }}</li>
+    @endforeach
+</ul>
 @stop
 
 @section('scripts')
 <script>
     jQuery(document).ready(function ($) {
+        /*
+        Pusher.log = function(message) {
+            if (window.console && window.console.log) {
+                window.console.log(message);
+            }
+        };
+        */
+        var $rdvs = $('#rdvs');
         var pusher = new Pusher('{{ env("PUSHER_KEY") }}');
 
         var rdvChannel = pusher.subscribe('rdv');
-        rdvChannel.bind('\\App\\Events\\RdvIsSaved', function (data) {
-            console.log('RdvIsSaved');
-            console.log(data);
+        rdvChannel.bind('App\\Events\\RdvIsSaved', function (data) {
+            $rdvs.append('<li>' + data.rdv.label + '</li>');
         });
     });
 </script>
